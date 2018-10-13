76990b413835bf7df48a4ed45b824de456e4cf02
Githubwyb <1061322005@qq.com>
2018-10-13 20:49:41 +0800

    添加两个排序算法

--- main.cpp
+++ main.cpp
@@ -7,16 +7,11 @@
 #include "log.hpp"
 
 #include <iostream>
+#include <vector>
 
+using namespace std;
 int main() {
-    LOG_DEBUG("Hello, world");
-    LOG_WARN("Hello, world");
-    LOG_INFO("Hello, world");
-    LOG_ERROR("Hello, world");
-    LOG_FATAL("Hello, world");
-    LOG_PRINT("Hello, world");
-    PRINT("Hello, world\r\n");
-    LOG_HEX("Hello, world", std::string("Hello, world").size());
-    LOG_BIN("Hello, world5555555555555555555555555555555555", std::string("Hello, world5555555555555555555555555555555555").size());
+    LOG_INFO("Hello, main");
+
     return 0;
 }
