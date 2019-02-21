18b69a4eeddcd47caa8dc3d00d2af5fd6e126fac
Githubwyb <1061322005@qq.com>
2019-02-21 22:48:23 +0800

    Merge branch 'master' of https://github.com/Githubwyb/Clion_cppStudy
    # Conflicts:
    #	log.cpp

--- main.cpp
+++ main.cpp
@@ -7,34 +7,64 @@
 #include "log.hpp"
 
 #include <iostream>
-#include <exception>
+#include <memory>
+#include <vector>
 
 using namespace std;
 
-class Trace {
-    static int counter;
-    int objid;
-public:
-    Trace() {
-        objid = counter++;
-        cout << "Constructing Trace #" << objid << endl;
-        if (objid == 3)
-            throw 3;
-    }
-    ~Trace() {
-        cout << "Destructing Trace #" << objid << endl;
-    }
-};
+typedef struct Node {
+    int value;
+    vector<shared_ptr<Node>> pChild;
+    weak_ptr<Node> pParent;
+} Node;
 
-int Trace::counter = 0;
+shared_ptr<Node> pRoot = nullptr;
+
+void addNodes();
 
 int main() {
-    try {
-        Trace n1;
-        //Throws exception;
-        Trace array[5];
-        Trace n2; //Won't get here.
-    } catch (int i) {
-        cout << "Caught " << i << endl;
-    }
+    pRoot = make_shared<Node>();
+    pRoot->value = 50;
+}
+
+void addNode(const shared_ptr<Node> &pParent, Node &pChild) {
+    pChild.pParent = pParent;
+    pParent->pChild.push_back(make_shared<Node>(pChild));
+}
+
+void clearNode(Node &node) {
+    node.pChild.clear();
+    node.pParent.reset();
+}
+
+void addNodes() {
+    Node tmp1;
+
+    clearNode(tmp1);
+    tmp1.value = 40;
+    addNode(pRoot, tmp1);
+
+    clearNode(tmp1);
+    tmp1.value = 30;
+    addNode(pRoot, tmp1);
+
+    clearNode(tmp1);
+    tmp1.value = 10;
+    addNode(pRoot, tmp1);
+
+    clearNode(tmp1);
+    tmp1.value = 20;
+    addNode(pRoot->pChild[0], tmp1);
+
+    clearNode(tmp1);
+    tmp1.value = 70;
+    addNode(pRoot->pChild[0], tmp1);
+
+    clearNode(tmp1);
+    tmp1.value = 15;
+    addNode(pRoot->pChild[0]->pChild[1], tmp1);
+
+    clearNode(tmp1);
+    tmp1.value = 25;
+    addNode(pRoot->pChild[0]->pChild[1], tmp1);
 }
