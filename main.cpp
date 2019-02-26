c5ff56b1cdf99c95822311d051ded1a60aca9e49
wangyubo <1061322005@qq.com>
2019-02-26 15:42:36 +0800

    暂存

--- main.cpp
+++ main.cpp
@@ -12,32 +12,54 @@
 using namespace std;
 
 int main() {
-    int n = 0;
+    int n, m, c;
     cin >> n;
-    vector<int> user;
+    cin >> m;
+    cin >> c;
+
+    int count[51] = {0};
+    bool colorNo[51] = {false};
+    vector<vector<int>> line;
     for (int i = 0; i < n; ++i) {
-        int like = 0;
-        cin >> like;
-        user.emplace_back(like);
-    }
-
-    int q = 0;
-    cin >> q;
-    for (int j = 0; j < q; ++j) {
-        int l = 0;
-        cin >> l;
-        int r = 0;
-        cin >> r;
-        int k = 0;
-        cin >> k;
-
-        int result = 0;
-        for (int i = l - 1; i < r; ++i) {
-            if (user[i] == k) {
-                result++;
+        int colorN = 0;
+        cin >> colorN;
+        vector<int> one;
+        if (i >= m) {
+            for (auto tmp : line[i - m]) {
+                count[tmp]--;
             }
         }
-        cout << result << endl;
+        for (int j = 0; j < colorN; ++j) {
+            int color = 0;
+            cin >> color;
+            if (count[color] == 1) {
+                colorNo[color] = true;
+            }
+            count[color]++;
+            one.emplace_back(color);
+        }
+        line.emplace_back(one);
     }
+
+    for (int k = 0; k < m; ++k) {
+        for (auto tmp : line[n + k - m]) {
+            count[tmp]--;
+        }
+        for (auto tmp : line[k]) {
+            if (count[tmp] == 1) {
+                colorNo[tmp] = true;
+            }
+            count[tmp]++;
+        }
+    }
+
+    int result = 0;
+    for (int l = 0; l < 51; ++l) {
+        if (colorNo[l]) {
+            result++;
+        }
+    }
+    cout << result;
+
     return 0;
 }
