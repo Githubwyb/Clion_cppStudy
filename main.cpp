ce1a2d9e8d225242e8db55461bc5ec1e7a6676be
wangyubo <1061322005@qq.com>
2019-02-26 16:59:16 +0800

    第二题

--- main.cpp
+++ main.cpp
@@ -8,31 +8,45 @@
 
 #include <iostream>
 #include <vector>
-#include <map>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int n = 0;
-    cin >> n;
-    map<int, vector<int>> likeMap;
+    int n, m, c;
+    cin >> n >> m >> c;
+
+    int last[51] = {0};     //上一次出现位置
+    int first[51] = {0};    //第一次出现位置
+    bool colorNo[51] = {false};
+
     for (int i = 0; i < n; ++i) {
-        int like = 0;
-        cin >> like;
-        likeMap[like].emplace_back(i + 1);
+        int colorN = 0;
+        cin >> colorN;
+        for (int j = 0; j < colorN; ++j) {
+            int color = 0;
+            cin >> color;
+            if (first[color] == 0) {
+                first[color] = i;
+            } else if (i - last[color] < m) {
+                colorNo[color] = true;
+            }
+            last[color] = i;
+        }
     }
 
-    int q = 0;
-    cin >> q;
-    for (int j = 0; j < q; ++j) {
-        int l, r, k;
-        cin >> l >> r >> k;
-
-        auto ln = lower_bound(likeMap[k].begin(), likeMap[k].end(), l);
-        auto rn = upper_bound(likeMap[k].begin(), likeMap[k].end(), r);
-        cout << rn - ln << endl;
+    for (int k = 1; k < 51; ++k) {
+        if (first[k] != 0 && last[k] + m - 1 - n >= first[k]) {
+            colorNo[k] = true;
+        }
     }
+
+    int result = 0;
+    for (int l = 0; l < 51; ++l) {
+        if (colorNo[l]) {
+            result++;
+        }
+    }
+    cout << result;
 
     return 0;
 }
