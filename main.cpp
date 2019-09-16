53a93e0214bc94ebffb929ce7704b800b618cd8a
wangyubo <18433@sangfor.com>
2019-09-16 10:57:19 +0800

    添加sqlite

--- main.cpp
+++ main.cpp
@@ -5,51 +5,70 @@
  */
 
 #include "log.hpp"
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "sqlite3.h"
+#include <string>
 #include <string.h>
-#include <stdio.h>
+#include <cassert>
 
-#define	UDP_TEST_PORT		80
+static int get_IOS_cert_callback(void *data, int cols, char **p_values, char **p_heads) {
+    assert(data != NULL && p_values != NULL);
+    if (p_values[0] != NULL) {
+        const char *nameStart = strstr(p_values[0], ": ");
+        if (nameStart == nullptr) {
+            nameStart = p_values[0];
+        } else {
+            nameStart += 2;
+        }
 
-int main(int argC, char* arg[])
-{
-    struct sockaddr_in addr;
-    int sockfd, len = 0;
-    socklen_t addr_len = sizeof(struct sockaddr_in);
-    char buffer[256];
-
-    /* 建立socket，注意必须是SOCK_DGRAM */
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror ("socket");
-        exit(1);
+        LOG_HEX(nameStart, strlen(nameStart));
+        strcpy((char *) data, nameStart);
+    } else {
+        memset(data, 0, 1);
     }
 
-    /* 填写sockaddr_in 结构 */
-    bzero(&addr, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(UDP_TEST_PORT);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY) ;// 接收任意IP发来的数据
+    return 0;
+}
 
-    /* 绑定socket */
-    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr))<0) {
-        perror("connect");
-        exit(1);
+
+//从数据库读取企业签名证书
+static int get_IOS_CorpSignCert(void *cert) {
+    // 构造sql语句
+    char buf_sql[] = "select subject from emm_cert_infos where cert_type=1";
+
+    // 从数据库中读取数据
+    sqlite3 *db;
+    int ret_sql = sqlite3_open("../sslvpn.sq3", &db);
+    if (ret_sql != SQLITE_OK) {
+        LOG_ERROR("open db error");
+        return -1;
+    } else {
+        LOG_DEBUG("open db success");
     }
 
-    while(1) {
-        bzero(buffer, sizeof(buffer));
-        len = recvfrom(sockfd, buffer, sizeof(buffer), 0,
-                       (struct sockaddr *)&addr ,&addr_len);
-        /* 显示client端的网络地址和收到的字符串消息 */
-        printf("Received from client %s:%d  : %s\n",
-               inet_ntoa(addr.sin_addr), addr.sin_port, buffer);
+    char *errorMsg = nullptr;
+    ret_sql = sqlite3_exec(db, buf_sql, get_IOS_cert_callback, cert, &errorMsg);
+    if (ret_sql != SQLITE_OK) {
+        LOG_ERROR("sqlite3_exec error, %s", errorMsg);
+        // 关闭数据库连接
+        sqlite3_close(db);
+        return -1;
+    } else {
+        LOG_DEBUG("sqlite3_exec success");
+    }
+    // 关闭数据库连接
+    sqlite3_close(db);
+    return 0;
+}
+
+
+int main(int argC, char *arg[]) {
+    char cert[128] = {0};
+
+    if (get_IOS_CorpSignCert(cert) != 0) {
+        LOG_ERROR("Get ios corpSignCert error");
+        return -1;
     }
 
+    LOG_DEBUG("cert: %s", cert);
     return 0;
 }
