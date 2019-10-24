5f11ab39d6c072d88fbbc33fa7704a0fe82be74e
wangyubo <18433@sangfor.com>
2019-10-24 10:55:45 +0800

    解决li正向st倒序插入vector

--- main.cpp
+++ main.cpp
@@ -12,30 +12,74 @@
 
 using namespace std;
 
+
+typedef struct ListNode {
+    int val;
+    struct ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {
+    }
+} ListNode;
+
 class Solution {
 public:
-    void replaceSpace(char *str,int length) {
-        int srcI = 0;
-        int desI = 0;
-        string desStr;
-        while (str != nullptr) {
-            char *find = strstr(str + srcI, " ");
-            if (find == nullptr)
-                break;
-            desStr.append(str + srcI, find);
-            srcI = find - str + 1;
-            desStr.append("%20");
+    vector<int> printListFromTailToHead(ListNode *head) {
+        vector<int> result;
+        //遍历查找最后一个元素，过程中倒序list
+        ListNode *pTmp1 = head;     //保存最开始的节点
+        ListNode *pTmp2 = nullptr;  //保存第二个
+        ListNode *pTmp3 = nullptr;  //保存第三个
+        if (pTmp1 == nullptr) {
+            return move(result);
         }
-        desStr.append(str + srcI, length - srcI);
-        memcpy(str, desStr.c_str(), desStr.size());
+        //循环列表
+        while (pTmp1->next != nullptr) {
+            //顺序后延一位
+            pTmp3 = pTmp2;
+            pTmp2 = pTmp1;
+            pTmp1 = pTmp1->next;
+
+            //第二个节点不为空的时候
+            if (pTmp2 != nullptr) {
+                pTmp2->next = pTmp3;
+            }
+        }
+
+        result.emplace_back(pTmp1->val);
+
+        while (pTmp2 != nullptr) {
+            result.emplace_back(pTmp2->val);
+            pTmp2 = pTmp2->next;
+        }
+
+        return move(result);
     }
 };
 
-int main(int argC, char *arg[])
-{
-    char test[255] = " i  l k lkj kk  kjl";
+void print_vector(const vector<int> &inV) {
+    LOG_DEBUG("Print");
+    for (auto tmp : inV) {
+        PRINT("%d ", tmp);
+    }
+    PRINT("\r\n");
+}
+
+int main(int argC, char *arg[]) {
+    ListNode test1(5);
+    ListNode test2(4);
+    ListNode test3(3);
+    ListNode test4(2);
+    ListNode test5(1);
+
     Solution a;
-    a.replaceSpace(test, strlen(test));
-    LOG_DEBUG("%s", test);
+//    print_vector(a.printListFromTailToHead(&test1));
+    test1.next = &test2;
+//    print_vector(a.printListFromTailToHead(&test1));
+    test2.next = &test3;
+//    print_vector(a.printListFromTailToHead(&test1));
+    test3.next = &test4;
+//    print_vector(a.printListFromTailToHead(&test1));
+    test4.next = &test5;
+    print_vector(a.printListFromTailToHead(&test1));
     return 0;
 }
