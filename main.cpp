40918e6d5828f56a85d307452eb7e761cbc94c1e
wangyubo <18433@sangfor.com>
2019-10-31 11:12:56 +0800

    解决链表倒数第k个值的问题

--- main.cpp
+++ main.cpp
@@ -14,52 +14,38 @@
 
 using namespace std;
 
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	explicit ListNode(int x) :
+			val(x), next(nullptr) {
+	}
 };
 class Solution {
 public:
-    static TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
-        if (pre.size() != vin.size()) {
-            return nullptr;
-        }
-        return reConstructBinaryTree(pre.begin().base(), vin.begin().base(), pre.size());
-    }
-
-private:
-    static TreeNode* reConstructBinaryTree(const int *pre, const int *vin, int size) {
-        if (size == 0) {
+    static ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+        if (k == 0) {
             return nullptr;
         }
 
-        //头结点从pre第一个取
-        auto head = new TreeNode(pre[0]);
-        //找到和vin中和pre[0]相等的点，左边为左子树，右边为右子树
-        for (int i = 0; i < size; ++i) {
-            if (vin[i] == pre[0]) {
-                head->left = reConstructBinaryTree(pre + 1, vin, i);
-                head->right = reConstructBinaryTree(pre + i + 1, vin + i + 1, size - i - 1);
-                break;
+        ListNode *p1 = pListHead;   //遍历指针
+        ListNode *p2 = pListHead;   //比p1提前k个节点的指针
+
+        int i = 0;
+        for (i = 1; p1 != nullptr; ++i) {
+            //走了k步后，p2移动跟上
+            if (i > k) {
+                p2 = p2->next;
             }
+            p1 = p1->next;
         }
 
-        return head;
+        //没走够k步返回空
+        return (i > k ? p2 : nullptr);
     }
 };
 
-void in_order_traversal(TreeNode *root) {
-    PRINT("%d, ", root->val);
-    if (root->left != nullptr)
-        in_order_traversal(root->left);
-    if (root->right != nullptr)
-        in_order_traversal(root->right);
-    delete root;
-}
-
 int main(int argC, char *arg[]) {
-    in_order_traversal(Solution::reConstructBinaryTree({1, 2, 4, 7, 3, 5, 6, 8}, {4, 7, 2, 1, 5, 3, 8, 6}));
     return 0;
 }
