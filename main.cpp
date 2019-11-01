6450a87097e1e0449d62c1787f14bb91768cda7b
wangyubo <18433@sangfor.com>
2019-11-01 12:02:39 +0800

    解决合并链表的问题

--- main.cpp
+++ main.cpp
@@ -15,19 +15,83 @@
 
 using namespace std;
 
-class Solution {
-public:
-    static double Power(double base, int exponent) {
-        unsigned int tmp = abs(exponent);
-        double r = 1.0;
-        while (tmp) {
-            if (tmp & (unsigned) 1) r *= base;
-            base *= base;
-            tmp >>= (unsigned) 1;
-        }
-        return exponent < 0 ? 1 / r : r;
+/*
+ * @Author WangYubo
+ * @Date 09/17/2018
+ * @Description
+ */
+
+#include <string>
+#include <vector>
+#include <stack>
+#include <memory>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include "log.hpp"
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+
+    explicit ListNode(int x) :
+            val(x), next(nullptr) {
     }
 };
+
+class Solution {
+public:
+    static ListNode *Merge(ListNode *pHead1, ListNode *pHead2) {
+        if (pHead1 == nullptr) {
+            return pHead2;
+        }
+
+        if (pHead2 == nullptr) {
+            return pHead1;
+        }
+
+        ListNode* result = nullptr;
+        if (pHead1->val < pHead2->val) {
+            result = pHead1;
+            pHead1 = pHead1->next;
+        } else {
+            result = pHead2;
+            pHead2 = pHead2->next;
+        }
+
+        ListNode* pTmp = result;
+        while (true) {
+            if (pHead1 == nullptr) {
+                pTmp->next = pHead2;
+                break;
+            }
+
+            if (pHead2 == nullptr) {
+                pTmp->next = pHead1;
+                break;
+            }
+
+            if (pHead1->val < pHead2->val) {
+                pTmp->next = pHead1;
+                pHead1 = pHead1->next;
+            } else {
+                pTmp->next = pHead2;
+                pHead2 = pHead2->next;
+            }
+            pTmp = pTmp->next;
+        }
+
+        return result;
+    }
+};
+
+int main(int argC, char *arg[]) {
+    return 0;
+}
+
 
 int main(int argC, char *arg[]) {
     return 0;
