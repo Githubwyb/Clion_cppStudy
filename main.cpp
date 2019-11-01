d397e9ded5b6535cd37f3a3ca1b9cf22deee2f71
wangyubo <18433@sangfor.com>
2019-11-01 11:37:42 +0800

    解决数值的整数次幂的问题

--- main.cpp
+++ main.cpp
@@ -17,38 +17,18 @@ using namespace std;
 
 class Solution {
 public:
-    static void reOrderArray(vector<int> &array) {
-        stable_partition(array.begin(), array.end(),
-                         [](const int &value) { return (value % 2 == 1); });
-        return;
-        vector<int> Odd;     //偶数队列
-        int size = array.size();
-        int j = 0;              //保存最后待放偶数的位置
-        for (int i = 0; i < size; ++i) {
-            if ((array[i] % 2) == 0) {
-                //LOG_DEBUG("push %d", array[i]);
-                Odd.emplace_back(array[i]);
-                continue;
-            }
-
-            if (i != j) {
-                //LOG_DEBUG("change %d to %d, value %d", j, i, array[i]);
-                array[j] = array[i];
-            }
-
-            ++j;
+    static double Power(double base, int exponent) {
+        unsigned int tmp = abs(exponent);
+        double r = 1.0;
+        while (tmp) {
+            if (tmp & (unsigned) 1) r *= base;
+            base *= base;
+            tmp >>= (unsigned) 1;
         }
-
-        memcpy(array.begin().base() + j, Odd.begin().base(), Odd.size() * sizeof(int));
+        return exponent < 0 ? 1 / r : r;
     }
 };
 
 int main(int argC, char *arg[]) {
-    vector<int> arr = {1, 2, 3, 3, 5, 6, 5, 4, 45, 54, 64};
-    Solution::reOrderArray(arr);
-
-    for (auto tmp : arr) {
-        PRINT("%d,  ", tmp);
-    }
     return 0;
 }
