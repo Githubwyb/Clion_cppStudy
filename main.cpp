989ab2cc5288a5e32f038482100e3ed46ec9ebd6
wangyubo <18433@sangfor.com>
2020-03-06 17:56:44 +0800

    二叉树中和为某一个值的路径

--- main.cpp
+++ main.cpp
@@ -12,20 +12,65 @@
 
 using namespace std;
 
+typedef struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode(int x, TreeNode *pLeft, TreeNode *pRight)
+        : val(x), left(pLeft), right(pRight) {}
+} TreeNode;
+
 class Solution {
    public:
-    static int FindGreatestSumOfSubArray(vector<int> array) {
-        int maxTail = -1;
-        int max = 0x80000000;
-        for (auto &tmp : array) {
-            maxTail = (maxTail + tmp) > tmp ? (maxTail + tmp) : tmp;
-            max = max < maxTail ? maxTail : max;
+    static vector<vector<int>> FindPath(TreeNode *root, int expectNumber) {
+        if (root == nullptr) {
+            return {};
         }
-        return max;
+        vector<vector<int>> result;
+        vector<int> road;
+        FindPath(root, expectNumber, result, road);
+        sort(result.begin(), result.end(), comp);
+        return result;
+    }
+
+   private:
+    static bool comp(vector<int> a, vector<int> b) {
+        return a.size() > b.size();
+    }
+
+    static void FindPath(TreeNode *root, int expectNumber,
+                         vector<vector<int>> &result, vector<int> &road) {
+        if (root == nullptr) {
+            return;
+        }
+
+        // sum增加，road放入此节点
+        expectNumber -= root->val;
+        road.emplace_back(root->val);
+
+        if (root->right == nullptr && root->left == nullptr &&
+            expectNumber == 0) {
+            // 查到根部，判断是否相等，相等则存入结果
+            result.emplace_back(road);
+        }
+
+        // 递归判断子节点
+        FindPath(root->right, expectNumber, result, road);
+        FindPath(root->left, expectNumber, result, road);
+
+        // 出递归需要还原路径
+        road.pop_back();
     }
 };
 
 int main() {
-    LOG_DEBUG("%d", Solution::FindGreatestSumOfSubArray(
-                        {1, -2, 3, 10, -4, 7, 2, -5}));
+    TreeNode root(10);
+    vector<vector<int>> result = Solution::FindPath(&root, 10);
+    for (auto &vTmp : result) {
+        for (auto &tmp : vTmp) {
+            PRINT("%d ", tmp);
+        }
+        PRINT("\r\n");
+    }
 }
