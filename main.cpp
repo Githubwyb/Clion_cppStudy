c5590e8f54d9d6df2107ab96ea22a517898f6fa5
wangyubo <18433@sangfor.com>
2020-03-09 20:34:27 +0800

    不区分大小写统计字符出现次数

--- main.cpp
+++ main.cpp
@@ -6,57 +6,29 @@
 
 #include <algorithm>
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <string>
+#include <vector>
+
 #include "log.hpp"
 
 using namespace std;
 
-typedef struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-    TreeNode(int x, TreeNode *pLeft, TreeNode *pRight)
-        : val(x), left(pLeft), right(pRight) {}
-} TreeNode;
-
-class Solution {
-   public:
-    // 递归实现
-    static int TreeDepth1(TreeNode *pRoot) {
-        if (pRoot == nullptr) {
-            return 0;
-        }
-        return max(TreeDepth1(pRoot->left) + 1, TreeDepth1(pRoot->right) + 1);
-    }
-
-    // 非递归实现，广度优先遍历
-    static int TreeDepth2(TreeNode *pRoot) {
-        if (pRoot == nullptr) {
-            return 0;
-        }
-
-        int maxDeep = 0;
-        queue<TreeNode *> traversal;
-        traversal.push(pRoot);
-        while(!traversal.empty()) {
-            int length = traversal.size();
-            for (int i = 0; i < length; i++)
-            {
-                auto &tmp = traversal.front();
-                if (tmp->left != nullptr) traversal.push(tmp->left);
-                if (tmp->right != nullptr) traversal.push(tmp->right);
-                traversal.pop();
-            }
-
-            maxDeep++;
-        }
-        return maxDeep;
-    }
-};
-
 int main() {
-    TreeNode root(10);
-    LOG_DEBUG("%d", Solution::TreeDepth2(&root));
+    std::string input;
+    getline(cin, input);
+    char findChr;
+    scanf("%c", &findChr);
+
+    int result = 0;
+    result += count(input.begin(), input.end(), findChr);
+    if (findChr >= 'A' && findChr <= 'Z') {
+        result += count(input.begin(), input.end(), findChr - 'A' + 'a');
+    }
+
+    if (findChr >= 'a' && findChr <= 'z') {
+        result += count(input.begin(), input.end(), findChr - 'a' + 'A');
+    }
+    cout << result;
+    return 0;
 }
