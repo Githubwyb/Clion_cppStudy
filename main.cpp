0b2c99433e9f7940a4a360a0d34113e900c9bdcf
wangyubo <18433@sangfor.com>
2020-03-09 20:16:48 +0800

    二叉树深度的问题

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
+#include <queue>
 #include "log.hpp"
 
 using namespace std;
@@ -23,54 +23,40 @@ typedef struct TreeNode {
 
 class Solution {
    public:
-    static vector<vector<int>> FindPath(TreeNode *root, int expectNumber) {
-        if (root == nullptr) {
-            return {};
+    // 递归实现
+    static int TreeDepth1(TreeNode *pRoot) {
+        if (pRoot == nullptr) {
+            return 0;
         }
-        vector<vector<int>> result;
-        vector<int> road;
-        FindPath(root, expectNumber, result, road);
-        sort(result.begin(), result.end(), comp);
-        return result;
+        return max(TreeDepth1(pRoot->left) + 1, TreeDepth1(pRoot->right) + 1);
     }
 
-   private:
-    static bool comp(vector<int> a, vector<int> b) {
-        return a.size() > b.size();
-    }
-
-    static void FindPath(TreeNode *root, int expectNumber,
-                         vector<vector<int>> &result, vector<int> &road) {
-        if (root == nullptr) {
-            return;
+    // 非递归实现，广度优先遍历
+    static int TreeDepth2(TreeNode *pRoot) {
+        if (pRoot == nullptr) {
+            return 0;
         }
 
-        // sum增加，road放入此节点
-        expectNumber -= root->val;
-        road.emplace_back(root->val);
+        int maxDeep = 0;
+        queue<TreeNode *> traversal;
+        traversal.push(pRoot);
+        while(!traversal.empty()) {
+            int length = traversal.size();
+            for (int i = 0; i < length; i++)
+            {
+                auto &tmp = traversal.front();
+                if (tmp->left != nullptr) traversal.push(tmp->left);
+                if (tmp->right != nullptr) traversal.push(tmp->right);
+                traversal.pop();
+            }
 
-        if (root->right == nullptr && root->left == nullptr &&
-            expectNumber == 0) {
-            // 查到根部，判断是否相等，相等则存入结果
-            result.emplace_back(road);
+            maxDeep++;
         }
-
-        // 递归判断子节点
-        FindPath(root->right, expectNumber, result, road);
-        FindPath(root->left, expectNumber, result, road);
-
-        // 出递归需要还原路径
-        road.pop_back();
+        return maxDeep;
     }
 };
 
 int main() {
     TreeNode root(10);
-    vector<vector<int>> result = Solution::FindPath(&root, 10);
-    for (auto &vTmp : result) {
-        for (auto &tmp : vTmp) {
-            PRINT("%d ", tmp);
-        }
-        PRINT("\r\n");
-    }
+    LOG_DEBUG("%d", Solution::TreeDepth2(&root));
 }
