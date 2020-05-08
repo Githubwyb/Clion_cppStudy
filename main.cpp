d7cb3af9ba77a0aed6432eca6d9f19657b2961e6
wangyubo <18433@sangfor.com>
2020-05-08 11:12:08 +0800

    查找重复数字

--- main.cpp
+++ main.cpp
@@ -13,50 +13,57 @@
 
 using namespace std;
 
-typedef struct ListNode {
-    int val;
-    struct ListNode *next;
-    ListNode(int x) :
-            val(x), next(NULL) {
-    }
-
-    ListNode(int x, ListNode *p) : val(x), next(p) {}
-} ListNode;
-
 class Solution {
    public:
-    // 最骚解法，最多两次遍历
-    ListNode *FindFirstCommonNode(ListNode *pHead1, ListNode *pHead2) {
-        auto p1 = pHead1;
-        auto p2 = pHead2;
-        // 要么相等，要么同时为nullptr
-        while (p1 != p2) {
-            p1 = (p1 == nullptr ? pHead2 : p1->next);
-            p2 = (p2 == nullptr ? pHead1 : p2->next);
+    // Parameters:
+    //        numbers:     an array of integers
+    //        length:      the length of array numbers
+    //        duplication: (Output) the duplicated number in the array number
+    // Return value:       true if the input is valid, and there are some
+    // duplications in the array number
+    //                     otherwise false
+    static bool duplicate(int numbers[], int length, int* duplication) {
+        for (int i = 0; i < length; i++) {
+            // 获取原始数据
+            auto tmp = numbers[i] < length ? numbers[i] : numbers[i] - length;
+            // 如果对应位小于length证明没有被访问过
+            if (numbers[tmp] < length) {
+                // 访问它，增加length
+                numbers[tmp] += length;
+                continue;
+            }
+            // 访问过，就是它了
+            *duplication = tmp;
+            return true;
         }
-        return p1;
+        return false;
     }
 
-    // 暴力破解
-    ListNode *FindFirstCommonNode1(ListNode *pHead1, ListNode *pHead2) {
-        auto p1 = pHead1;
-        auto p2 = pHead2;
-        // 要么相等，要么同时为nullptr
-        while (p1 != p2) {
-            // p1到尾部，p2后移
-            p2 = (p1 == nullptr ? p2 : p2->next);
-            // p1到尾部，从头再来
-            p1 = (p1 == nullptr ? pHead1 : p1->next);
+    // Parameters:
+    //        numbers:     an array of integers
+    //        length:      the length of array numbers
+    //        duplication: (Output) the duplicated number in the array number
+    // Return value:       true if the input is valid, and there are some
+    // duplications in the array number
+    //                     otherwise false
+    static bool duplicate1(int numbers[], int length, int* duplication) {
+        vector<bool> count(length, false);
+        for (int i = 0; i < length; i++) {
+            if (count[numbers[i]]) {
+                *duplication = numbers[i];
+                return true;
+            }
+            count[numbers[i]] = true;
         }
-        return p1;
+        return false;
     }
 };
 
 int main() {
-   // (void)BugReportRegister("run", ".", nullptr, nullptr);
-    auto common = new ListNode(6, new ListNode(7, nullptr));
-    ListNode test1 = ListNode(1, new ListNode(2, new ListNode(3, common)));
-    ListNode test2 = ListNode(4, new ListNode(5, common));
-    LOG_DEBUG("%d", common == Solution::FindFirstCommonNode(&test1, &test2));
+    // (void)BugReportRegister("run", ".", nullptr, nullptr);
+    int test[] = {2, 4, 3, 1, 4};
+    int p = -1;
+    auto tmp = Solution::duplicate(test, sizeof(test) / sizeof(test[0]), &p);
+    LOG_DEBUG("%d %d", tmp, p);
     return 0;
 }
