9461efbb7f7ccfbb7b5c29e7853ec8ae5c8ccdaa
wangyubo <18433@sangfor.com>
2020-05-08 19:44:34 +0800

    统计一个数字在排序数组中出现的次数

--- main.cpp
+++ main.cpp
@@ -15,55 +15,17 @@ using namespace std;
 
 class Solution {
    public:
-    // Parameters:
-    //        numbers:     an array of integers
-    //        length:      the length of array numbers
-    //        duplication: (Output) the duplicated number in the array number
-    // Return value:       true if the input is valid, and there are some
-    // duplications in the array number
-    //                     otherwise false
-    static bool duplicate(int numbers[], int length, int* duplication) {
-        for (int i = 0; i < length; i++) {
-            // 获取原始数据
-            auto tmp = numbers[i] < length ? numbers[i] : numbers[i] - length;
-            // 如果对应位小于length证明没有被访问过
-            if (numbers[tmp] < length) {
-                // 访问它，增加length
-                numbers[tmp] += length;
-                continue;
-            }
-            // 访问过，就是它了
-            *duplication = tmp;
-            return true;
-        }
-        return false;
-    }
-
-    // Parameters:
-    //        numbers:     an array of integers
-    //        length:      the length of array numbers
-    //        duplication: (Output) the duplicated number in the array number
-    // Return value:       true if the input is valid, and there are some
-    // duplications in the array number
-    //                     otherwise false
-    static bool duplicate1(int numbers[], int length, int* duplication) {
-        vector<bool> count(length, false);
-        for (int i = 0; i < length; i++) {
-            if (count[numbers[i]]) {
-                *duplication = numbers[i];
-                return true;
-            }
-            count[numbers[i]] = true;
-        }
-        return false;
+    static int GetNumberOfK(vector<int> data, int k) {
+        auto tmp =
+            equal_range(data.begin(), data.end(), k,
+                        [](int a, int b) { return (a > b); });
+        return tmp.second - tmp.first;
     }
 };
 
 int main() {
     // (void)BugReportRegister("run", ".", nullptr, nullptr);
-    int test[] = {2, 4, 3, 1, 4};
-    int p = -1;
-    auto tmp = Solution::duplicate(test, sizeof(test) / sizeof(test[0]), &p);
-    LOG_DEBUG("%d %d", tmp, p);
+    auto tmp = Solution::GetNumberOfK({5, 4, 3, 3, 3, 2, 1}, 3);
+    LOG_DEBUG("%d", tmp);
     return 0;
 }
