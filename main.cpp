41dc71b5630cfef0030907326d67ae80883869d0
wangyubo <1061322005@qq.com>
2020-07-12 21:17:02 +0800

    完成观察者模式

--- main.cpp
+++ main.cpp
@@ -4,54 +4,95 @@
  * @Description
  */
 
-#include <algorithm>
+#include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
+#include "bugReport.hpp"
 #include "log.hpp"
-//#include "bugReport.hpp"
 
 using namespace std;
 
-class Solution {
+class Object;
+
+class Observer {
    public:
-    static void FindNumsAppearOnce(vector<int> data, int* num1, int* num2) {
-        int nor = 0;
-        // 先把所有数字异或，结果为两个单独出现的数字的异或
-        for (auto &tmp : data) {
-            nor ^= tmp;
-        }
+    Observer(string key, int value) : m_key(key), m_value(value) {}
+    // 外部接口，通知到观察者的操作
+    void update(Object *poj);
 
-        int bitNum = 0;
-        // 找到第一个不同的位
-        for (int i = 0; i < 32; ++i) {
-            if (nor & 0x01) {
-                bitNum = i;
-                break;
-            }
-            nor >>= 1;
-        }
-        bitNum = 1 << bitNum;
-        
-        // 根据此位将所有数字分成两组进行异或，最终得到结果
-        *num1 = 0;
-        *num2 = 0;
-        for (auto &tmp : data) {
-            if (tmp & bitNum) {
-                *num1 ^= tmp;
-                continue;
-            }
+   private:
+    string m_key;
+    int m_value;
+};
 
-            *num2 ^= tmp;
+
+// 被观察者
+class Object {
+   public:
+    int m_value;
+    // 添加一位观察者
+    void attach(string key, Observer *p_ob) {
+        if (m_ob.find(key) != m_ob.end()) {
+            LOG_WARN("%s has been attached", key.c_str());
+            return;
+        }
+        m_ob[key] = p_ob;
+        LOG_DEBUG("attach %s", key.c_str());
+    }
+
+    // 删除观察
+    void deattach(string key) {
+        auto it = m_ob.find(key);
+        if (it != m_ob.end()) {
+            m_ob.erase(it);
+            LOG_DEBUG("erase %s", key);
+        }
+    }
+
+    // 更新值
+    void set_value(int value) {
+        m_value = value;
+        notify();
+    }
+
+   private:
+    map<string, Observer *> m_ob;   // 观察者map
+    // 通知所有观察者执行操作
+    void notify() {
+        for (auto it = m_ob.begin(); it != m_ob.end(); it++) {
+            it->second->update(this);
         }
     }
 };
 
+void Observer::update(Object *poj) {
+    if (m_value == poj->m_value) {
+        LOG_DEBUG("%s, hhh", m_key.c_str());
+    }
+}
+
 int main() {
-    // (void)BugReportRegister("run", ".", nullptr, nullptr);
-    int a = -1;
-    int b = -1;
-    Solution::FindNumsAppearOnce({2, 4, 3, 6, 3, 2, 5, 5}, &a, &b);
-    LOG_DEBUG("%d %d", a, b);
+    (void)BugReportRegister("run", ".", nullptr, nullptr);
+    Object obj;
+    Observer a("aObserver", 1);
+    Observer b("bObserver", 2);
+    Observer c("cObserver", 3);
+    Observer d("dObserver", 4);
+
+    // 观察者注册到被观察者
+    obj.attach("a", &a);
+    obj.attach("b", &b);
+    obj.attach("c", &c);
+    obj.attach("a", &a);
+
+    while (true) {
+        /* code */
+        int value;
+        cin >> value;
+        obj.set_value(value);
+    }
+
     return 0;
 }
