ac56b4ff9b9249499e3c830dfbe8316e6cde450a
wangyubo <18433@sangfor.com>
2020-08-04 20:53:11 +0800

    线程池完善到单独文件

--- main.cpp
+++ main.cpp
@@ -4,107 +4,23 @@
  * @Description
  */
 
-#include <sys/time.h>
-
-#include <atomic>
-#include <condition_variable>
-#include <functional>
-#include <future>
-#include <memory>
-#include <mutex>
-#include <queue>
-#include <string>
-#include <thread>
-#include <vector>
-
 #include "baseInstance.hpp"
+#include "threadPool.hpp"
 #include "log.hpp"
 
+#include <atomic>
+#include <vector>
+#include <future>
+
 using namespace std;
-using Task = function<void(void)>;
-static const int s_threadNum = 4;  //线程数量
+
+class testPool : public threadPool, public BaseInstance<testPool> {
+   public:
+    void init(int threadNum) { threadPool::init(threadNum, "testPool"); }
+};
 
 atomic<int> addNum;
 atomic<int> result;
-
-class threadPool : public BaseInstance<threadPool> {
-   private:
-    vector<thread> m_pool;        //线程池
-    atomic<int> m_idleThreadNum;  //空闲线程数量
-    atomic<bool> m_stoped;        //是否停止线程
-    mutex m_lock;                 //线程池锁
-    queue<Task> m_tasks;          //待执行任务
-    condition_variable m_cv;      //线程控制
-    //线程执行函数
-    void run();
-
-   public:
-    //构造函数
-    threadPool();
-
-    ~threadPool();
-
-    //添加线程函数
-    template <class F, class... Args>
-    auto commit(F &&f, Args &&... args) -> future<decltype(f(args...))>;
-};
-
-threadPool::threadPool() {
-    m_stoped = false;
-    for (size_t i = 0; i < s_threadNum; i++) {
-        m_pool.emplace_back(thread(&threadPool::run, this));
-    }
-}
-
-threadPool::~threadPool() {
-    m_stoped = true;
-    m_cv.notify_all();
-    for (auto &tmp : m_pool) {
-        if (tmp.joinable()) tmp.join();
-    }
-}
-
-template <class F, class... Args>
-auto threadPool::commit(F &&f, Args &&... args)
-    -> future<decltype(f(args...))> {
-    using RetType = decltype(f(args...));
-
-    if (m_stoped) {
-        LOG_INFO("ThreadPool has been stoped");
-        return future<RetType>();
-    }
-
-    auto task = make_shared<packaged_task<RetType()>>(
-        bind(forward<F>(f), forward<Args>(args)...));
-    auto ret = task->get_future();
-    {
-        lock_guard<mutex> lock(m_lock);
-        m_tasks.emplace([task] { (*task)(); });
-    }
-    m_cv.notify_one();
-    return ret;
-}
-
-void threadPool::run() {
-    while (true) {
-        m_idleThreadNum--;
-        Task task;
-        {
-            unique_lock<mutex> lock(m_lock);
-            m_cv.wait(lock,
-                      [this] { return this->m_stoped || !(m_tasks.empty()); });
-            if (m_tasks.empty()) {
-                return;
-            }
-            task = move(m_tasks.front());
-            m_tasks.pop();
-        }
-        LOG_DEBUG("Handle one task");
-        task();
-        m_idleThreadNum++;
-    }
-}
-
 int test() {
     int add = 0;
     while (true) {
@@ -118,7 +34,8 @@ int test() {
 
 int main(int argC, char *arg[]) {
     LOG_DEBUG("Hello");
-    threadPool &pool = threadPool::getInstance();
+    testPool &pool = testPool::getInstance();
+    pool.init(5);
     vector<future<int>> ret;
 
     for (int i = 0; i < 20; i++) {
--- threadPool.cpp
+++ threadPool.cpp
@@ -3,4 +3,59 @@
 //
 
 #include "threadPool.hpp"
-#include <thread>
+
+#include "log.hpp"
+
+using namespace std;
+
+threadPool::~threadPool() {
+    m_stoped = true;
+    m_cv.notify_all();
+    for (auto &tmp : m_pool) {
+        if (tmp.joinable()) tmp.join();
+    }
+}
+
+void threadPool::init(int threadNum, string name) {
+    if (!m_stoped) {
+        LOG_INFO("%s has been started, thread num %d", m_poolName.c_str(),
+                 m_threadNum);
+        return;
+    }
+
+    {
+        unique_lock<mutex> lock(m_lock);
+        if (!m_stoped) {
+            return;
+        }
+        m_stoped = false;
+        // 清理旧的线程，初始化新的线程
+        m_pool.clear();
+
+        for (int i = 0; i < threadNum; i++) {
+            m_pool.emplace_back(thread(&threadPool::run, this));
+        }
+        m_threadNum = threadNum;
+        m_poolName = name;
+    }
+    LOG_INFO("%s start thread num %d", m_poolName.c_str(), m_threadNum);
+}
+
+void threadPool::run() {
+    while (true) {
+        m_idleThreadNum--;
+        Task task;
+        {
+            unique_lock<mutex> lock(m_lock);
+            m_cv.wait(lock, [this] { return m_stoped || !(m_tasks.empty()); });
+            if (m_tasks.empty()) {
+                return;
+            }
+            task = move(m_tasks.front());
+            m_tasks.pop();
+        }
+        LOG_DEBUG("Handle one task");
+        task();
+        m_idleThreadNum++;
+    }
+}
