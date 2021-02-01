be1ed4028ceed607654d67f9d8b87efee7b812f2
wangyubo <1061322005@qq.com>
2021-02-01 14:32:23 +0800

    添加shmhandler

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ mkdir -p "$OUTPUTLIBDIR"
 
 # build lib
 # libhv
-if [ ! -f "src/threepart/libhv/lib/libhv.a" ]; then
-    cd src/threepart/libhv
+if [ ! -f "threepart/libhv/lib/libhv.a" ]; then
+    cd threepart/libhv
     bash configure
     make
     if [ "$?" -ne 0 ]; then
@@ -22,17 +22,29 @@ if [ ! -f "src/threepart/libhv/lib/libhv.a" ]; then
     cd -
 fi
 # sdplog
-if [ ! -f "src/threepart/spdlog/build/libspdlog.a" ]; then
-    mkdir src/threepart/spdlog/build
-    cd src/threepart/spdlog/build
+if [ ! -f "threepart/spdlog/build/libspdlog.a" ]; then
+    mkdir threepart/spdlog/build
+    cd threepart/spdlog/build
     cmake .. && make
     if [ "$?" -ne 0 ]; then
-        echo "Build libhv failed"
-        rm -f "lib/libhv.a"
+        echo "Build libspdlog failed"
+        rm -f "threepart/spdlog/build/libspdlog.a"
         exit
     fi
     cd -
 fi
+# subhook
+if [ ! -f "threepart/subhook/libsubhook.so" ]; then
+    cd threepart/subhook
+    cmake . && make
+    if [ "$?" -ne 0 ]; then
+        echo "Build libsubhook failed"
+        rm -f "threepart/subhook/libsubhook.so"
+        exit
+    fi
+    cd -
+fi
+cp "threepart/subhook/libsubhook.so" "$OUTPUTLIBDIR"
 
 # build main program
 make
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,30 +5,61 @@
  * @version v2.0.0
  * @date 2020-12-16
  */
-
 #include <iostream>
+#include <string>
+#include <vector>
 
+#include "baseInstance.hpp"
 #include "bugReport.hpp"
-#include "libutils/circleList.hpp"
+#include "common.hpp"
 #include "log.hpp"
-#include "shmManager.hpp"
-#include "epollWorker.hpp"
+#include "threadPool.hpp"
 
 using namespace std;
+
+class testPool : public threadPool, public BaseInstance<testPool> {
+   public:
+    void init(int threadNum) { threadPool::init(threadNum, "testPool"); }
+    void testHHH(int a) { LOG_INFO("a {}", a); }
+};
+
+class testClass {
+   public:
+    int testFunc(char *buf, int size, const char *pStrIp, testPool *b) {
+        LOG_INFO("get param buf: {:p}, size: {}", buf, size);
+        b->testHHH(size);
+        return 0;
+    }
+};
+
+int run(char *buf, int size, const char *pStrIp, testPool *b) {
+    testClass obj;
+    return obj.testFunc(buf, size, pStrIp, b);
+}
+
+extern "C" void init_mylib(void *);
 
 int main(int argC, char *argV[]) {
     (void)BugReportRegister("run", ".", nullptr, nullptr);
     spdlog::set_level(spdlog::level::debug);  // Set global log level to debug
     spdlog::set_pattern("%Y-%m-%d %H:%M:%S [%P:%t][%L][%@ %!] %v");
+    auto pFunc = (void *)&testClass::testFunc;
+    init_mylib(pFunc);
+    LOG_INFO("start, want hook func {:p}", pFunc);
+    auto &pool = testPool::getInstance();
+    char test[] = "tessss";
+    pool.init(5);
+    // 定义返回值
+    vector<future<int>> ret;
 
-    if (argC > 1 && strcmp(argV[1], "client") == 0) {
-        LOG_DEBUG("Client mod");
-        clientRun();
-    } else {
-        LOG_DEBUG("Server mod");
-        serverRun();
+    for (int i = 0; i < 20; i++) {
+        // 插入任务函数
+        ret.emplace_back(pool.commit(run, test, 6, "abdc", &pool));
     }
-
+    for (auto &tmp : ret) {
+        // 等待结果返回
+        tmp.wait();
+    }
     return 0;
 }
 
