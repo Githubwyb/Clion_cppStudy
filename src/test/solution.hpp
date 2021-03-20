f5072613993525907db8db851a0deaa7f07ed6a1
wangyubo <1061322005@qq.com>
2021-03-20 14:59:38 +0800

    完成LRU缓存算法

--- src/test/solution.hpp
+++ src/test/solution.hpp
@@ -6,38 +6,75 @@
  * @date 2021-03-17
  */
 
-#include <stack>
-
-struct ListNode {
-    int val;
-    struct ListNode *next;
-};
+#include <list>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
+class lru_cache {
+   public:
+    lru_cache(int k) : cache_size(k) {
+        m_lruList.clear();
+        m_cache.clear();
+    }
+    void set(int key, int value) {
+        // 先查找value
+        auto it = m_cache.find(key);
+        if (it != m_cache.end()) {
+            // 找到，先删除，后面插入
+            m_lruList.erase(it->second);
+        } else if (m_lruList.size() >= cache_size) {
+            // 没找到判断是否超过最大个数，删除最后一个，map也要删除
+            auto delIt = m_lruList.back();
+            m_cache.erase(delIt.first);
+            m_lruList.pop_back();
+        }
+
+        m_lruList.emplace_front(make_pair(key, value));
+        m_cache[key] = m_lruList.begin();
+    }
+
+    int get(int key) {
+        // 先查找value
+        auto it = m_cache.find(key);
+        if (it == m_cache.end()) {
+            // 没找到返回-1
+            return -1;
+        }
+
+        // 找到，将缓存插入到最前面
+        m_lruList.splice(m_lruList.begin(), m_lruList, it->second);
+        // 这里返回值
+        return (*(it->second)).second;
+    }
+
+   private:
+    int cache_size;                  // cache最大大小
+    list<pair<int, int>> m_lruList;  // 用于存放lru的链表，值为value
+    unordered_map<int, list<pair<int, int>>::iterator>
+        m_cache;  // 用于O(1)查找的map
+};
+
 class Solution {
    public:
-    bool hasCycle(ListNode *head) {
-		/* 思路1
-         * 想象两个人在捉迷藏，一个人先走到一个地方，另一个人去找
-         * 如果找到他的时候和他走的路程一样长，那就是原路找的
-         * 如果比他路程还短，说明另外一个人绕了一圈，也就是有环路
-         *
-         * 思路2（当前实现代码）
-         * 两个人追逐，一个走的快（一次2步），一个走的慢（一次一步）
-         * 如果相遇，说明有环路
-         *
-         * 思路3
-         * 破坏链表，遍历的同时，让前一个节点指向头指针
-         * 遍历如果到了头指针就说明有环路
-		 */
-        ListNode *pFast = head;     // 跑的快的指针
-        ListNode *pSlow = head;     // 跑得慢的指针
-        while (pFast != nullptr && pFast->next != nullptr) {
-            pSlow = pSlow->next;
-            pFast = pFast->next->next;
-            if (pSlow == pFast) return true;
+    /**
+     * lru design
+     * @param operators int整型vector<vector<>> the ops
+     * @param k int整型 the k
+     * @return int整型vector
+     */
+    vector<int> LRU(vector<vector<int>>& operators, int k) {
+        // write code here
+        lru_cache lru(k);
+        vector<int> result;
+        for (auto& item : operators) {
+            if (item[0] == 1) {
+                lru.set(item[1], item[2]);
+            } else if (item[0] == 2) {
+                result.emplace_back(lru.get(item[1]));
+            }
         }
-        return false;
+        return result;
     }
 };
