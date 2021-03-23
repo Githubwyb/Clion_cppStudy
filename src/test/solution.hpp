41a16afb30b31837c10a2d2ebfc64d5f5e101764
wangyubo <1061322005@qq.com>
2021-03-23 16:11:10 +0800

    完成二叉树之字型遍历（一个栈一个队列）

--- src/test/solution.hpp
+++ src/test/solution.hpp
@@ -8,27 +8,55 @@
 
 #include <stack>
 #include <string>
+#include <vector>
+
 using namespace std;
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
 
 class Solution {
    public:
-    void threeOrders1(TreeNode *root, vector<vector<int>> &result) {
-        if (root == nullptr) return;
-        result[0].emplace_back(root->val);
-        threeOrders1(root->left, result);
-        result[1].emplace_back(root->val);
-        threeOrders1(root->right, result);
-        result[2].emplace_back(root->val);
-    }
     /**
      *
-     * @param root TreeNode类 the root of binary tree
+     * @param root TreeNodeÀà
      * @return int整型vector<vector<>>
      */
-    vector<vector<int>> threeOrders(TreeNode *root) {
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root) {
         // write code here
-        vector<vector<int>> result = {{}, {}, {}};
-        threeOrders1(root, result);
+        if (root == nullptr) return {};
+        vector<vector<int>> result;
+        stack<TreeNode *> order;  // 顺序层
+        bool isReverse = false;
+        // 先放一个根节点
+        order.emplace(root);
+        while (!order.empty()) {
+            vector<TreeNode *> current;
+            // 顺序层插入，左节点开始
+            int stackSize = order.size();
+            while (stackSize--) {
+                TreeNode *tmp = order.top();
+                order.pop();
+                // 当前层插入
+                current.emplace_back(tmp);
+            }
+            vector<int> floor;
+            for (auto &tmp : current) {
+                if (isReverse) {
+                    if (tmp->right) order.emplace(tmp->right);
+                    if (tmp->left) order.emplace(tmp->left);
+                } else {
+                    if (tmp->left) order.emplace(tmp->left);
+                    if (tmp->right) order.emplace(tmp->right);
+                }
+                floor.emplace_back(tmp->val);
+            }
+            isReverse = !isReverse;
+            if (!floor.empty()) result.emplace_back(floor);
+        }
         return result;
     }
 };
--- src/test/test_algorithm.cpp
+++ src/test/test_algorithm.cpp
@@ -6,8 +6,6 @@
  * @date 2021-03-17
  */
 
-#include <vector>
-
 #include "log.hpp"
 #include "solution.hpp"
 #include "xtest/xtest.h"
@@ -17,6 +15,40 @@ using namespace std;
 //  完成使用场景的测试
 TEST(test, scene) {
     Solution so;
-    EXPECT_EQ(so.isValid("{{(([[]]))}}"), true);
+    TreeNode root;
+    root.val = 1;
+    TreeNode mid1;
+    mid1.val = 2;
+    TreeNode mid2;
+    mid2.val = 3;
+    TreeNode last1;
+    last1.val = 4;
+    TreeNode last2;
+    last2.val = 5;
+
+    root.left = &mid1;
+    root.right = &mid2;
+
+    mid1.left = &last1;
+    mid1.right = nullptr;
+    mid2.left = nullptr;
+    mid2.right = &last2;
+
+    last1.right = nullptr;
+    last1.left = nullptr;
+    last2.right = nullptr;
+    last2.left = nullptr;
+
+    auto result = so.zigzagLevelOrder(&root);
+    EXPECT_TRUE(result.size() == 3);
+    EXPECT_TRUE(result[0].size() == 1);
+    EXPECT_TRUE(result[1].size() == 2);
+    EXPECT_TRUE(result[2].size() == 2);
+
+    EXPECT_TRUE(result[0][0] == 1);
+    EXPECT_TRUE(result[1][0] == 3);
+    EXPECT_TRUE(result[1][1] == 2);
+    EXPECT_TRUE(result[2][0] == 4);
+    EXPECT_TRUE(result[2][1] == 5);
 }
 
