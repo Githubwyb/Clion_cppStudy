2dcf83cb5c499e7e14f874645e431324525b85ac
wangyubo <1061322005@qq.com>
2021-03-23 22:51:50 +0800

    完成二叉树之字型遍历（两个栈）

--- src/test/solution.hpp
+++ src/test/solution.hpp
@@ -29,33 +29,34 @@ class Solution {
         // write code here
         if (root == nullptr) return {};
         vector<vector<int>> result;
-        stack<TreeNode *> order;  // 顺序层
-        bool isReverse = false;
+        stack<TreeNode *> order;    // 顺序层
+        stack<TreeNode *> reverse;  // 倒序层
         // 先放一个根节点
         order.emplace(root);
         while (!order.empty()) {
-            vector<TreeNode *> current;
+            vector<int> current;
             // 顺序层插入，左节点开始
             int stackSize = order.size();
             while (stackSize--) {
                 TreeNode *tmp = order.top();
                 order.pop();
                 // 当前层插入
-                current.emplace_back(tmp);
+                current.emplace_back(tmp->val);
+                if (tmp->left) reverse.emplace(tmp->left);
+                if (tmp->right) reverse.emplace(tmp->right);
             }
-            vector<int> floor;
-            for (auto &tmp : current) {
-                if (isReverse) {
-                    if (tmp->right) order.emplace(tmp->right);
-                    if (tmp->left) order.emplace(tmp->left);
-                } else {
-                    if (tmp->left) order.emplace(tmp->left);
-                    if (tmp->right) order.emplace(tmp->right);
-                }
-                floor.emplace_back(tmp->val);
+            if (!current.empty()) result.emplace_back(current);
+            current.clear();
+            stackSize = reverse.size();
+            while (stackSize--) {
+                TreeNode *tmp = reverse.top();
+                reverse.pop();
+                // 当前层插入
+                current.emplace_back(tmp->val);
+                if (tmp->right) order.emplace(tmp->right);
+                if (tmp->left)  order.emplace(tmp->left);
             }
-            isReverse = !isReverse;
-            if (!floor.empty()) result.emplace_back(floor);
+            if (!current.empty()) result.emplace_back(current);
         }
         return result;
     }
