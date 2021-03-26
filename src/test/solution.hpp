9b1bb9f90bdfcca9897d6e6157b846b20c6f1603
wangyubo <1061322005@qq.com>
2021-03-26 15:33:50 +0800

    完成二分查找

--- src/test/solution.hpp
+++ src/test/solution.hpp
@@ -6,29 +6,46 @@
  * @date 2021-03-17
  */
 
+#include <algorithm>
 #include <list>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 class Solution {
    public:
-    int findKth(vector<int> a, int n, int K) {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 如果目标值存在返回下标，否则返回 -1
+     * @param nums int整型vector
+     * @param target int整型
+     * @return int整型
+     */
+    int search(vector<int>& nums, int target) {
         // write code here
-        /* 思路
-         * 小根堆实现
-         */
-        // 构建小根堆
-        auto cmp = [] (int &a1, int &b) { return a1 > b; };
-        make_heap(a.begin(), a.begin() + K, cmp);
-        for (int i = K; i < n; ++i) {
-            // 如果第n个值大于堆顶，堆顶移出，插入新值
-            if (a[i] > a.front()) {
-                swap(a[K], a[i]);
-                pop_heap(a.begin(), a.begin() + K + 1, cmp);
-            }
+        int len = nums.size();
+        int left = 0;
+        int right = len - 1;
+
+        // check array boundary
+        if (len == 0 || target < nums.front() || target > nums[right])
+            return -1;
+        if (target == nums.front()) return 0;
+        // left move faster than mid and mid is more close to left, finally left
+        // will be equal or bigger than right
+        while (left < right) {
+            int mid = (right + left) >> 1;  // 0-3 mid 1, more close to left
+            // fast convergence, mid +- 1
+            if (target > nums[mid])
+                left = mid + 1;  // left - 1 must less than target
+            else
+                right = mid - 1;  // right + 1 maybe equal target
         }
-        return a.front();
+        // if left bigger than right, judge left only
+        // if left == right, judge left and right + 1
+        if (nums[left] == target) return left;
+        if (left == right && nums[++left] == target) return left;
+        return -1;
     }
 };
--- src/test/test_algorithm.cpp
+++ src/test/test_algorithm.cpp
@@ -15,6 +15,7 @@ using namespace std;
 //  完成使用场景的测试
 TEST(test, scene) {
     Solution so;
-    EXPECT_EQ(so.findKth({1,3,5,2,2}, 5, 3), 2);
+    vector<int> test1 = {0, 1, 2, 3, 4};
+    EXPECT_EQ(so.search(test1, 2), 2);
 }
 
