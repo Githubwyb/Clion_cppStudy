880e607d54d14eeda9b4ecfe86f118272097ed7c
wangyubo <1061322005@qq.com>
2021-03-30 19:29:23 +0800

    完成两数之和

--- src/test/solution.hpp
+++ src/test/solution.hpp
@@ -15,64 +15,22 @@ using namespace std;
 class Solution {
    public:
     /**
-     * @brief 转递归函数实现，a传入指针方便递归
      *
-     * @param a 头指针
-     * @param n 数组长度
-     * @param K K值
-     *
-     * @return 返回第K大的值
+     * @param numbers int整型vector
+     * @param target int整型
+     * @return int整型vector
      */
-    int findKth(int *a, int n, int K) {
-        assert(K > 0 && n > 0);
-        assert(n >= K);
-        // use the first item to be the axis
-        int axis = a[0];
-        int left = 0;
-        int right = n - 1;
-        // First, the right run, when right > axis, left = right, then left run
-        // when left < axis, right = left, then right run
-        // equal won't handle
-        // with this way, we can avoid swap item too frequently
-        bool isRightRun = true;
-        while (left != right) {
-            // right run
-            if (isRightRun) {
-                // right less than axis
-                if (a[right] <= axis) {
-                    right--;
-                    continue;
-                }
-                a[left] = a[right];
-                isRightRun = false;
-                continue;
-            }
-            // left run
-            // left bigger than axis
-            if (a[left] >= axis) {
-                left++;
-                continue;
-            }
-            a[right] = a[left];
-            isRightRun = true;
-        }
-        a[left] = axis;
-        // if left is K
-        if ((left + 1) == K) return axis;
-        // left bigger than K, use right part to sort
-        if ((left + 1) < K) return findKth(a + left + 1, n - 1 - left, K - left - 1);
-        // left bigger than K, use right part to sort
-        return findKth(a, left, K);
-    }
-
-    int findKth(vector<int> a, int n, int K) {
+    vector<int> twoSum(vector<int>& numbers, int target) {
         // write code here
-        /* 思路1
-         * 小根堆实现
-         *
-         * 思路2（当前）
-         * 快排思想
-         */
-        return findKth(&a[0], n, K);
+        int len = numbers.size();
+        for (int i = 0; i < len - 1; ++i) {
+            int tg = target - numbers[i];
+            for (int j = i + 1; j < len; ++j) {
+                if (tg == numbers[j]) {
+                    return {i + 1, j + 1};
+                }
+            }
+        }
+        return {};
     }
 };
